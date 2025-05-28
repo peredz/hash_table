713e9e66468fb9880b72e516a5adb1682a348f19
pzaha <pzaharkin53@gmail.com>
2025-05-28 14:16:34 +1000

    IT'S WORKING!! All left is just pretty

--- hash_table.cpp
+++ hash_table.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <string>
 #include <iomanip>
-#include <algorithm>
 
 class Record
 {
@@ -43,6 +42,12 @@ public:
     void printRest() const
     {
         std::cout << " " << carInfo;
+    }
+
+    void print() const
+    {
+        printKey();
+        printRest();
     }
 };
 
@@ -107,168 +112,149 @@ public:
 private:
     using ValueType = T;
 
-    static const inline int DEFAULT_SIZE = 8;
+    static const int DEFAULT_SIZE = 8;
     static constexpr double LOAD_FACTOR = 0.75;
     int bufferSize;
     int size;
     int usedSlots;
-    Node **table;
-
-    void initializeTable(Node **tbl, int size)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            tbl[i] = nullptr;
-        }
-    }
+    Node *table;
 
     bool insert(ValueType value, bool rehash = false)
     {
         Hash1 h1;
         Hash2 h2;
         int initialIndex = h1(value, bufferSize);
-        Node *newNode = new Node(std::move(value));
+        Node newNode(std::move(value));
 
-        if (table[initialIndex] == nullptr)
+        if (table[initialIndex].status != 1)
         {
-            table[initialIndex] = newNode;
-            size++;
+            if (table[initialIndex].status == 0)
+            {
+                size++;
+            }
             if (!rehash)
                 usedSlots++;
-            if (!rehash)
-                checkForResize();
-            return true;
-        }
-        else if (table[initialIndex]->status != 1)
-        {
-            delete table[initialIndex];
             table[initialIndex] = newNode;
-            size++;
-            if (!rehash)
-                usedSlots++;
             if (!rehash)
                 checkForResize();
             return true;
         }
 
         int j = 1;
-        int probeIndex;
-        do
+        int probeIndex = initialIndex;
+        while (table[probeIndex].status == 1)
         {
-            probeIndex = h2(initialIndex, j) % bufferSize;
-
-            if (table[probeIndex] == nullptr)
+            if (table[probeIndex].value.compareKeys(newNode.value))
             {
-                table[probeIndex] = newNode;
-                size++;
-                if (!rehash)
-                    usedSlots++;
-                if (!rehash)
-                    checkForResize();
-                return true;
-            }
-            else if (table[probeIndex]->status != 1)
-            {
-                delete table[probeIndex];
-                table[probeIndex] = newNode;
-                size++;
-                if (!rehash)
-                    usedSlots++;
-                if (!rehash)
-                    checkForResize();
-                return true;
-            }
-            else if (table[probeIndex]->value.compareKeys(newNode->value))
-            {
-                delete newNode;
                 return false;
             }
-
+            probeIndex = h2(initialIndex, j) % bufferSize;
             j++;
-        } while (j < bufferSize);
+        }
 
-        delete newNode;
-        return false;
+        table[probeIndex] = newNode;
+
+        if (table[probeIndex].status == 0)
+        {
+            size++;
+        }
+        if (!rehash)
+            usedSlots++;
+        if (!rehash)
+            checkForResize();
+        return true;
     }
 
     void checkForResize()
     {
-        double loadFactor = static_cast<double>(usedSlots) / bufferSize;
+        double actualLoadPercentage = double(usedSlots) / double(bufferSize);
 
-        if (loadFactor > LOAD_FACTOR ||
-            (loadFactor < (1 - LOAD_FACTOR) && bufferSize > DEFAULT_SIZE))
+        Node *old_table = table;
+        int old_size = bufferSize;
+
+        if (actualLoadPercentage > LOAD_FACTOR)
         {
-            int newSize = (loadFactor > LOAD_FACTOR) ? bufferSize * 2 : bufferSize / 2;
-            newSize = std::max(newSize, DEFAULT_SIZE);
+            resize(true);
+        }
+        else if (actualLoadPercentage < (1 - LOAD_FACTOR))
+        {
+            resize(false);
+        }
+        else
+        {
+            return;
+        }
 
-            Node **newTable = new Node *[newSize];
-            initializeTable(newTable, newSize);
+        table = new Node[bufferSize];
 
-            int oldSize = bufferSize;
-            Node **oldTable = table;
+        rehash(old_table, old_size);
+        delete[] old_table;
+    }
 
-            table = newTable;
-            bufferSize = newSize;
-            size = 0;
-            usedSlots = 0;
-
-            for (int i = 0; i < oldSize; i++)
-            {
-                if (oldTable[i] != nullptr && oldTable[i]->status == 1)
-                {
-                    insert(oldTable[i]->value, true);
-                }
-                delete oldTable[i];
-            }
-            delete[] oldTable;
+    void resize(bool grow)
+    {
+        if (grow)
+        {
+            bufferSize = bufferSize * 2;
+            return;
+        }
+        if (bufferSize > DEFAULT_SIZE)
+        {
+            bufferSize = bufferSize / 2;
         }
     }
 
-    Node *findNode(ValueType value)
+    void rehash(Node *old_table, int old_size)
+    {
+        for (int i = 0; i < old_size; i++)
+        {
+            if (old_table[i].status == 1)
+            {
+                insert(old_table[i].value, true);
+            }
+        }
+        size = usedSlots;
+    }
+
+    Node findeNode(ValueType value, bool needToBeDeteleted = false)
     {
         Hash1 h1;
         Hash2 h2;
         int initialIndex = h1(value, bufferSize);
 
-        if (table[initialIndex] != nullptr &&
-            table[initialIndex]->status == 1 &&
-            table[initialIndex]->value.compareKeys(value))
+        if (table[initialIndex].value.compareKeys(value) && table[initialIndex].status == 1)
         {
+            if (needToBeDeteleted)
+                table[initialIndex].disable();
             return table[initialIndex];
         }
 
         int j = 1;
-        int probeIndex;
-        while (j < bufferSize)
+        int probeIndex = initialIndex;
+        while (table[probeIndex].status != 0)
         {
-            probeIndex = h2(initialIndex, j) % bufferSize;
-
-            if (table[probeIndex] == nullptr)
-                break;
-
-            if (table[probeIndex]->status == 1 &&
-                table[probeIndex]->value.compareKeys(value))
+            if (table[probeIndex].value.compareKeys(value) && table[probeIndex].status == 1)
             {
+                if (needToBeDeteleted)
+                    table[probeIndex].disable();
                 return table[probeIndex];
             }
-
-            if (table[probeIndex]->status == 0)
-                break;
-
+            probeIndex = h2(initialIndex, j) % bufferSize;
             j++;
         }
 
-        return nullptr;
+        return Node();
     }
 
-    bool deleteElement(Record recordToDelete)
+    bool deteleElement(Record recordToDelete)
     {
-        Node *nodeToDelete = findNode(recordToDelete);
+        Node nodeToDelete = findeNode(recordToDelete, true);
 
-        if (nodeToDelete == nullptr || nodeToDelete->status != 1)
+        if (nodeToDelete.status == 0)
+        {
             return false;
+        }
 
-        nodeToDelete->disable();
         usedSlots--;
         checkForResize();
         return true;
@@ -277,16 +263,11 @@ private:
 public:
     HashTable() : bufferSize(DEFAULT_SIZE), size(0), usedSlots(0)
     {
-        table = new Node *[DEFAULT_SIZE];
-        initializeTable(table, DEFAULT_SIZE);
+        table = new Node[DEFAULT_SIZE];
     }
 
     ~HashTable()
     {
-        for (int i = 0; i < bufferSize; i++)
-        {
-            delete table[i];
-        }
         delete[] table;
     }
 
@@ -304,32 +285,26 @@ public:
         {
             std::cout << std::setw(6) << i;
 
-            if (table[i] == nullptr)
+            if (table[i].status == 0)
             {
                 std::cout << std::setw(25) << "no data"
                           << std::setw(25) << "-"
                           << std::setw(12) << "empty";
             }
-            else if (table[i]->status == 0)
+            else if (table[i].status == 1)
             {
-                std::cout << std::setw(25) << "no data"
-                          << std::setw(25) << "-"
-                          << std::setw(12) << "empty";
-            }
-            else if (table[i]->status == 1)
-            {
-                std::string keyStr = table[i]->value.fio + " " + std::to_string(table[i]->value.number);
+                std::string keyStr = table[i].value.fio + " " + std::to_string(table[i].value.number);
                 std::cout << std::setw(25) << keyStr
-                          << std::setw(25) << table[i]->value.carInfo
+                          << std::setw(25) << table[i].value.carInfo
                           << std::setw(12) << "occupied";
             }
             else // status == 2
             {
-                std::cout << std::setw(25) << "deleted"
+                std::string keyStr = table[i].value.fio + " " + std::to_string(table[i].value.number);
+                std::cout << std::setw(25) << keyStr
                           << std::setw(25) << "-"
                           << std::setw(12) << "deleted";
             }
-
             std::cout << "\n";
         }
         std::cout << "Buffer size: " << bufferSize
@@ -345,44 +320,86 @@ public:
 
     int find(const std::string &fio, int number)
     {
-        Record recordToFind(fio, number);
-        Node *node = findNode(recordToFind);
-        return node ? node->value.lineNumber : -1;
+        Record recordToFinde(fio, number);
+        return findeNode(recordToFinde).value.lineNumber;
     }
 
     bool remove(const std::string &fio, int number)
     {
         Record recordToDelete(fio, number);
-        return deleteElement(recordToDelete);
+        return deteleElement(recordToDelete);
     }
 };
 
 int main()
 {
     HashTable<Record> table;
-    std::cout << "Initial empty table:\n";
     table.print();
-
-    std::cout << "Adding 6 elements...\n";
-    for (int i = 0; i < 6; i++)
+    std::cout << std::endl;
+    for (int i = 0; i < 10; i++)
     {
-        table.add(Record("JohnDoe", "ToyotaCamry", "08:30", 100 + i));
+        if (table.add(Record("JohnSmith", "ToyotaCamry", "08:30", 490 + i)))
+        {
+            std::cout << "Added ToyotaCamry " << 490 + i << ":\n";
+        }
+        else
+            std::cout << "Error occurred while adding\n";
     }
     table.print();
 
-    std::cout << "Adding 4 more elements to trigger resize...\n";
-    for (int i = 6; i < 10; i++)
+    if (table.add(Record("JohnSmith", "ToyotaCamry", "08:30", 493)))
     {
-        table.add(Record("JohnDoe", "HondaCivic", "09:15", 100 + i));
+        std::cout << "Added " << 493 << ":\n";
     }
+    else
+        std::cout << "\n\nError occurred while adding\n";
     table.print();
 
-    std::cout << "Removing 5 elements to trigger downsizing...\n";
+    for (int i = 5; i < 12; i += 2)
+    {
+        if (table.remove("JohnSmith", 490 + i))
+        {
+            std::cout << "\nJohnSmith - " << 490 + i << " was successfully removed\n\r";
+        }
+        else
+            std::cout << "\n\nError occurred while removing\n";
+        table.print();
+    }
+
     for (int i = 0; i < 5; i++)
     {
-        table.remove("JohnDoe", 100 + i);
+        if (table.remove("JohnSmith", 490 + i))
+        {
+            std::cout << "\nJohnSmith - " << 490 + i << " was successfully removed\n\r";
+        }
+        else
+            std::cout << "\n\nError occurred while removing\n";
+        table.print();
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (table.add(Record("JohnSmith", "ToyotaCamry", "08:30", 490 + i)))
+        {
+            std::cout << "Added ToyotaCamry " << 490 + i << ":\n";
+        }
+        else
+            std::cout << "Error occurred while adding\n";
     }
     table.print();
 
+    for (int i = 40; i < 55; i++)
+    {
+        if (table.remove("JohnSmith", 490 + i))
+        {
+            std::cout << "\nJohnSmith - " << 490 + i << " was successfully removed\n\r";
+        }
+        else
+            std::cout << "\n\nError occurred while removing\n";
+    }
+    table.print();
+
+    // 231   JohnSmith 541            ToyotaCamry              occupied
+    // не удалило, хотя должно было и не писало ошибки
     return 0;
 }
